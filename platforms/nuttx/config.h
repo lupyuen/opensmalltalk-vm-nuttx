cf95eb105cc58850b1afc8d44baa8cde953c55c7
Lee Lup Yuen <luppy@appkaki.com>
2023-05-12 20:12:46 +0800

    Fixing build for macOS

--- platforms/nuttx/config.h
+++ platforms/nuttx/config.h
@@ -1,2 +1,11 @@
 // Assume 64-bit platform
 #define SIZEOF_VOID_P 8
+
+// TODO: Verify
+#define HAVE_TM_GMTOFF
+
+// TODO
+#define OS_TYPE "unix"
+#define VM_TARGET_OS "TODO:VM_TARGET_OS"
+#define VM_TARGET_CPU "TODO:VM_TARGET_CPU"
+#define VM_BUILD_STRING "TODO:VM_BUILD_STRING"
--- run.sh
+++ run.sh
@@ -7,4 +7,22 @@ gcc \
   -I include \
   src/v3.stack/gcc3x-interp.c \
   src/v3.stack/interp.c \
+  platforms/minheadless/generic/sqPlatformSpecific-Generic.c \
+  platforms/unix/vm/sqUnixMain.c \
+  platforms/minheadless/common/sqExternalPrimitives.c \
+  platforms/minheadless/common/sqWindow-Null.c \
   platforms/minheadless/common/sqMain.c \
+  platforms/minheadless/common/sqInternalPrimitives.c \
+  platforms/minheadless/common/sqWindow-Dispatch.c \
+  platforms/minheadless/common/sqVirtualMachineInterface.c \
+  platforms/minheadless/common/sqPrinting.c \
+  platforms/minheadless/common/version.c \
+  platforms/minheadless/common/sqEventCommon.c \
+  platforms/minheadless/unix/sqUnixHeartbeat.c \
+  platforms/minheadless/unix/sqPlatformSpecific-Unix.c \
+  platforms/minheadless/unix/sqUnixCharConv.c \
+  platforms/minheadless/unix/sqUnixThreads.c \
+  platforms/minheadless/unix/sqUnixSpurMemory.c \
+  platforms/minheadless/unix/sqUnixMemory.c \
+
+#  platforms/minheadless/unix/aioUnix.c \
